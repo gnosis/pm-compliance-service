6b3281e24c1bcdd5afbf5eca0fe720776f11df20
Giacomo Licari <giacomo.licari@gmail.com>
2019-06-10 18:30:58 +0200

    Add test serializers

---
+++ pm_compliance_service/compliance/tests/test_serializers.py
@@ -0,0 +1,42 @@
+from django.conf import settings
+from django.urls import reverse
+from eth_account import Account
+from gnosis.eth.tests.ethereum_test_case import EthereumTestCaseMixin
+from rest_framework import status
+from rest_framework.exceptions import ErrorDetail
+from django.test import TestCase
+
+from .factories import get_mocked_signup_data
+from ..models import User, Status
+from ..serializers import UserModelSerializer
+
+
+class TestSerializers(TestCase, EthereumTestCaseMixin):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.prepare_tests()
+
+    def setUp(self):
+        self.last_snapshot_idx = self.w3.manager.request_blocking('evm_snapshot', [])
+
+    def tearDown(self):
+        self.w3.manager.request_blocking("evm_revert", [self.last_snapshot_idx])
+
+    def test_user_serializer(self):
+        ethereum_address = Account.create().address
+        custom_data = {'user':{'ethereum_address': ethereum_address}}
+        mock_data = get_mocked_signup_data(**custom_data)  # override default data
+
+        self.send_ether(ethereum_address, settings.MIN_SIGNUP_ETH_BALANCE)  # put money on eth address
+
+        serializer = UserModelSerializer(data=mock_data['user'])
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        instance = serializer.save()
+        self.assertIsNotNone(instance)
+
+        # serialize django model
+        user = User.objects.first()
+        serialized_data = UserModelSerializer(user)
+        self.assertEqual(serialized_data.data['email'], mock_data['user']['email'])
+
